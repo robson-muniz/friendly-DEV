2dd69c2b68b79ba6d7a79bb7750673b4ddf9d565
robson-muniz <robson_muniz@yahoo.com>
2025-12-20 12:03:48 +0000

    Initial Commits

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,14 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "The Friendly DEV" },
+    { name: "description", content: "Custom Website Developer" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <>
+    My App
+  </>;
 }
