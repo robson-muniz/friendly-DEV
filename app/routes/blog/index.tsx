c538e13ccb162fa5f468cea6bace76308b8edf2b
robson-muniz <robson_muniz@yahoo.com>
2026-01-01 10:38:08 +0000

    feat(posts): fetch post metadata

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,4 +1,22 @@
-const BlogPage = () => {
+import type { Route } from "./+types/index";
+import type { PostMeta } from "./+types/index";
+import { Link } from "react-router";
+
+
+export async function loader({request}: Route.LoaderArgs) : Promise <{posts: PostMeta[]}> {
+    const url = new URL('/posts-meta.json', request.url);
+    const res = await fetch(url.href);
+
+    if (!res.ok) throw new Error('Failed to fetch posts metadata');
+
+    const data = await res.json()
+
+    return {posts: data}
+}
+
+const BlogPage = ({loaderData}: Route.ComponentProps) => {
+    const {posts} = loaderData;
+    console.log(posts)
     return (
         <>
             <h2 className="text-3xl text-white font-bold mb-8">
